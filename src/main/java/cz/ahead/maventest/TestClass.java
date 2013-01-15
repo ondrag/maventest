631d0ffcc50299117845bb702c48f74376cf72bf
Ondra <ondrej.gabrhelik@monetplus.cz>
2013-01-15 12:56:23 +0100

    prvni commit

---
+++ src/main/java/cz/ahead/maventest/TestClass.java
@@ -0,0 +1,14 @@
+package cz.ahead.maventest;
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nazdar svete");
+
+	}
+
+}
